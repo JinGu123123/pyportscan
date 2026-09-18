c572420a3bd5262710ebea2696d52bff250e97e4
s2220254491 <GuJin123123@outlook.com>
2026-09-18 09:40:27 +0800

    阶段0-3：实现基础的多线程端口扫描器
    - 阶段1：单端口扫描（socket.connect_ex）
    - 阶段2：多端口扫描，实测 1029s，验证单线程瓶颈
    - 阶段3：ThreadPoolExecutor 并发，1029s -> 6s，提速 170x

---
+++ main.py
@@ -0,0 +1,46 @@
+import socket
+from concurrent.futures import ThreadPoolExecutor
+
+
+"""单线程单端口"""
+def scan_port(host,port,timeout = 1.0):
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#settimeout保证若超时connect_ex会跳过，继续下一个
+    sock.settimeout(timeout)
+    try:
+        #connect_ex失败会返回一个错误码（成功返回0，失败返回其他），接受一个参数，传入元组
+        result = sock.connect_ex((host,port))
+        return result == 0
+    finally:
+        sock.close()
+
+"""多线程多端口"""
+def scan_ports(host,ports,timeout = 1.0,max_workers = 200):
+    open_ports = []
+
+    """创建线程池
+    with退出时自动关闭线程池"""
+    with ThreadPoolExecutor(max_workers = max_workers) as excutor:
+        """futures等待结果
+        (…):port for port in ports字典推导式,key:future;value:port
+        """
+        futures = {excutor.submit(scan_port,host,port,timeout):port
+                   for port in ports}
+        for future in futures:
+            port = futures[future]
+            print(future.done())
+            if future.result():
+                print(future.done())
+                open_ports.append(port)
+
+    open_ports.sort()
+    return open_ports
+
+if __name__ == "__main__":
+    import time
+    start = time.time()
+    result = scan_ports('192.168.31.21',range(1,1025),timeout = 1.0,max_workers = 200)
+    elpased = time.time() - start
+    print(f"开放端口：{result}")
+    print(f"扫描时间：{elpased:.2f}秒")
